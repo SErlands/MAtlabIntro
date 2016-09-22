0256dcecfbdade1a951aa6d733c284a38a760d93
freni244 <fredrik777@telia.com>
2016-09-22 21:54:27 +0200

    allocate_seat + beskrivning av båda funktionerna
    allocate_seat är en funktion som löser uppgift 2.3

---
+++ Main.m
@@ -0,0 +1,29 @@
+%%Main script were we store things that will be used in more then one
+%%function
+
+clear
+load('euelection2014.mat')
+
+
+% Take out the data for each party
+M=euvalet2014(:,1);
+C=euvalet2014(:,2);
+FP=euvalet2014(:,3);
+KD=euvalet2014(:,4);
+PP=euvalet2014(:,5);
+MP=euvalet2014(:,6);
+SD=euvalet2014(:,7);
+FI=euvalet2014(:,8);
+S=euvalet2014(:,9);
+V=euvalet2014(:,10);
+Other=euvalet2014(:,11);
+
+% Labels
+parties = {'M', 'C', 'FP', 'KD', 'PP', 'MP', 'SD', 'FI', 'S', 'V', 'Other'};
+votes = sum(euvalet2014); % array of total votes for each party
+seats = [0 0 0 0 0 0 0 0 0 0 0];
+
+electionresults(euvalet2014, parties)
+
+allocate_seat(votes, seats)
+%allocate_seat([4 208 3 208], [0 1 0 1])
---
+++ electionresults.m
@@ -0,0 +1,12 @@
+function [] = electionresults(election, parties)
+% Creates a pie chart showing the election result.
+% Inarguments:  election - matrix containing the election results.
+%               parties  - cell structure containing the names of each party.
+
+%%  2.2 Visualisation of the election results
+
+pie(sum(election))
+legend(parties, 'Location', 'NorthwestOutside')
+
+
+
