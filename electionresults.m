239cb23651c8a4c0eca52896212a7c7a2bb62d10
Samuel Erlands <samer437@student.liu.se>
2016-09-27 08:58:26 +0200

    sdzfgaszfg

--- electionresults.m
+++ electionresults.m
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
-function [] = electionresults(election, parties)
+function [] = electionresults(val, parties)
 % Creates a pie chart showing the election result.
 % Inarguments:  election - matrix containing the election results.
-%               parties  - cell structure containing the names of each party.
+%               parties  - cell structure containing the names of each party
 
-%%  2.2 Visualisation of the election results
-
-pie(sum(election))
-=======
-function [] = electionresults(val, parties)
 
 %%  2.2 Visualisation of the election results
 
 
 pie(val)
->>>>>>> refs/remotes/origin/master
+
 legend(parties, 'Location', 'NorthwestOutside')
 
 
